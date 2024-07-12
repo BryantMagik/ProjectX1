211fa03347673eaca15598cd38183acca5fed9d9
HFPRUZ <heflaruz@gmail.com>
2024-07-12 11:41:53 +0200

    intento de dashboard con tailwindcss

--- frontend-angular/src/app/pages/dashboard/dashboard.component.ts
+++ frontend-angular/src/app/pages/dashboard/dashboard.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BoardComponent } from '../../shared/board/board.component';
+import { SidebarComponent } from '../../shared/sidebar/sidebar.component';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [CommonModule, BoardComponent],
+  imports: [CommonModule, BoardComponent, SidebarComponent],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
