7f64beef933ceb0276da4ef707b6573a9dceca48
HFPRUZ <heflaruz@gmail.com>
2024-07-19 14:12:50 +0200

    "dashboardv1_3"

--- frontend-angular/src/app/shared/projects/projects.component.ts
+++ frontend-angular/src/app/shared/projects/projects.component.ts
@@ -1,12 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, NgModule} from '@angular/core';
+import { CommonModule, NgFor } from '@angular/common';
 
+
+interface Project {
+  id: number;
+  name: string;
+  description: string;
+  codeKey: string;
+  type: string;
+  status: string;
+  creationDate: string;
+  updateDate: string;
+  createdBy: string;
+  participants: string[];
+}
 @Component({
   selector: 'app-projects',
   standalone: true,
-  imports: [],
+  imports: [NgFor,CommonModule],
   templateUrl: './projects.component.html',
   styleUrl: './projects.component.css'
 })
-export class ProjectsComponent {
+
+
+
+export class ProjectsComponent implements OnInit {
+  projects: Project[] = [
+    {
+      id: 1,
+      name: 'Project Alpha',
+      description: 'This is a description for Project Alpha.',
+      codeKey: 'ALP123',
+      type: 'software',
+      status: 'Active',
+      creationDate: '2023-07-01',
+      updateDate: '2024-07-01',
+      createdBy: 'John Doe',
+      participants: ['Jane Smith', 'Bob Johnson']
+    },
+    // Añade más proyectos aquí
+  ];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
 
 }
