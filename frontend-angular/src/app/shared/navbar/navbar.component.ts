473e69016ece6ad86126d26baee62a066a8bfc8c
HFPRUZ <heflaruz@gmail.com>
2024-08-05 14:10:58 +0200

    "iniciocambionavbar"

--- frontend-angular/src/app/shared/navbar/navbar.component.ts
+++ frontend-angular/src/app/shared/navbar/navbar.component.ts
@@ -14,12 +14,29 @@ export class NavbarComponent implements OnInit {
   mostrar: number = 0;
   textos: string[] = ['Home', 'Profiles', 'Projects', 'Tasks', 'Subtasks', 'Issues', 'Comments'];
 
+  mirando: number = 0;
+  textos2: string[] = [' ','Projects-Details', 'Tasks-Details', 'Subtasks-Details', 'Issues-Details', 'Comments-Details'];
+ 
   constructor(private LayoutComponent: LayoutComponent) {}
+
+  setMostrar(value: number) {
+    this.LayoutComponent.setMostrar(value);
+  }
+
+  setMirando(value: number) {
+    this.LayoutComponent.setMirando(value);
+  }
 
   ngOnInit() {
     this.LayoutComponent.mostrar$.subscribe(valor => {
-      this.mostrar = valor;
+        this.mostrar = valor;
     });
+
+
+    this.LayoutComponent.mirando$.subscribe(valor => {
+      this.mirando = valor;
+    });
+
   }
   
 }
