fc3fb320b0ee041d064d3f8bff50025325ccc864
HFPRUZ <heflaruz@gmail.com>
2024-08-22 12:48:00 +0200

    "boardcambio"

--- frontend-angular/src/app/app.config.ts
+++ frontend-angular/src/app/app.config.ts
@@ -1,10 +1,10 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideHttpClient, withFetch } from '@angular/common/http';
+import { provideCharts, withDefaultRegisterables } from 'ng2-charts';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(), provideHttpClient(withFetch())]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(), provideHttpClient(withFetch()), provideCharts(withDefaultRegisterables())]
 };
--- frontend-angular/src/app/shared/board/board.component.ts
+++ frontend-angular/src/app/shared/board/board.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TicketComponent } from '../ticket/ticket.component';
 import { CommentsComponent } from '../comments/comments.component';
